aa2cf927bd841d9245e5c452eb139ba41110a907
Maxim <hitfm00@gmail.com>
2024-09-01 23:08:50 +0300

    feat: add notifications and tanstack react-query to MainLayout

--- client/src/layouts/MainLayout/index.tsx
+++ client/src/layouts/MainLayout/index.tsx
@@ -1,8 +1,11 @@
 import "@/styles/reset.scss";
 import "@mantine/core/styles.css";
+import "@mantine/notifications/styles.css";
 
+import { Notifications } from "@mantine/notifications";
 import { MantineProvider } from "@mantine/core";
 import { FC } from "react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 import { theme } from "@/theme";
 
@@ -11,10 +14,23 @@ type Props = {
   children: React.ReactNode;
 };
 
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      staleTime: 1000 * 60 * 60,
+    },
+  },
+});
+
 export const MainLayout: FC<Props> = ({ children }) => {
   return (
-    <MantineProvider theme={theme} withCssVariables>
-      {children}
-    </MantineProvider>
+    <QueryClientProvider client={queryClient}>
+      <MantineProvider theme={theme} withCssVariables>
+        <Notifications />
+
+        {children}
+      </MantineProvider>
+    </QueryClientProvider>
   );
 };
